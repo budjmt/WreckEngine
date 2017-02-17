54417931a0873f1a49759e907e015865934fbbf2
Richard Selneck <raselneck@gmail.com>
2017-02-17 15:10:27 -0500

    Convert tabs to spaces in DrawMesh.cpp, add overloads for DrawMesh that accept existing textures

--- WreckEngine/DrawMesh.cpp
+++ WreckEngine/DrawMesh.cpp
@@ -1,44 +1,49 @@
 #include "DrawMesh.h"
 
 DrawMesh::DrawMesh(Render::MaterialPass* r, shared<Mesh> m, const char* texFile, GLprogram shader) : _mesh(m) { renderer = r; setup(texFile, shader); }
-
+DrawMesh::DrawMesh(Render::MaterialPass* r, shared<Mesh> m, GLtexture tex, GLprogram shader) : _mesh(m) { renderer = r; setup(tex, shader); }
 DrawMesh::DrawMesh(Render::MaterialPass* r, Mesh::FaceData& fd, Mesh::FaceIndex& fi, const char* texFile, GLprogram shader) : DrawMesh(r, make_shared<Mesh>(fd, fi), texFile, shader) {}
+DrawMesh::DrawMesh(Render::MaterialPass* r, Mesh::FaceData& fd, Mesh::FaceIndex& fi, GLtexture tex, GLprogram shader) : DrawMesh(r, make_shared<Mesh>(fd, fi), tex, shader) {}
 
 void DrawMesh::setup(const char* texFile, GLprogram shader) {
-	
-	auto renderData = _mesh->getRenderData();
+    setup(genTexture2D(texFile), shader);
+}
 
-	vArray.create();
-	vArray.bind();
+void DrawMesh::setup(GLtexture tex, GLprogram shader) {
+    
+    auto renderData = _mesh->getRenderData();
 
-	vertBuffer.create(GL_ARRAY_BUFFER);
-	vertBuffer.bind();
-	vertBuffer.data(sizeof(GLfloat) * renderData->vbuffer.size(), &renderData->vbuffer[0]);
+    vArray.create();
+    vArray.bind();
 
-	elBuffer.create(GL_ELEMENT_ARRAY_BUFFER);
-	elBuffer.bind();
-	elBuffer.data(sizeof(GLuint) * renderData->ebuffer.size(), &renderData->ebuffer[0]);
+    vertBuffer.create(GL_ARRAY_BUFFER);
+    vertBuffer.bind();
+    vertBuffer.data(sizeof(GLfloat) * renderData->vbuffer.size(), &renderData->vbuffer[0]);
 
-	//set up an attribute for how the coordinates will be read
-	GLattrarr attrSetup;
-	attrSetup.add<GLfloat>(FLOATS_PER_VERT);
-	attrSetup.add<GLfloat>(FLOATS_PER_UV);
-	attrSetup.add<GLfloat>(FLOATS_PER_NORM);
-	//enable attributes
-	attrSetup.apply();
+    elBuffer.create(GL_ELEMENT_ARRAY_BUFFER);
+    elBuffer.bind();
+    elBuffer.data(sizeof(GLuint) * renderData->ebuffer.size(), &renderData->ebuffer[0]);
 
-	vArray.unbind();
+    //set up an attribute for how the coordinates will be read
+    GLattrarr attrSetup;
+    attrSetup.add<GLfloat>(FLOATS_PER_VERT);
+    attrSetup.add<GLfloat>(FLOATS_PER_UV);
+    attrSetup.add<GLfloat>(FLOATS_PER_NORM);
+    //enable attributes
+    attrSetup.apply();
 
-	shader.use();
-	worldMatrix   = GLresource<mat4>(shader, "worldMatrix");
-	iTworldMatrix = GLresource<mat4>(shader, "iTworldMatrix");
-	_color = GLresource<vec4>(shader, "tint");
+    vArray.unbind();
+
+    shader.use();
+    worldMatrix   = GLresource<mat4>(shader, "worldMatrix");
+    iTworldMatrix = GLresource<mat4>(shader, "iTworldMatrix");
+    _color = GLresource<vec4>(shader, "tint");
 
     material.setShaders(shader, &worldMatrix, &iTworldMatrix, &_color);
-    material.setTextures(genTexture2D(texFile));
+    material.setTextures(tex);
 }
 
 void DrawMesh::draw(const mat4& world) {
-	Drawable::draw(world);
+    Drawable::draw(world);
     renderer->scheduleDrawElements(renderGroup, &vArray, &material, tesselPrim, _mesh->getRenderData()->ebuffer.size(), GLtype<uint32_t>());
 }
--- WreckEngine/DrawMesh.h
+++ WreckEngine/DrawMesh.h
@@ -6,9 +6,12 @@
 class DrawMesh : public Drawable {
 public:
     DrawMesh(Render::MaterialPass* r, shared<Mesh> m, const char* texFile, GLprogram shader);
+    DrawMesh(Render::MaterialPass* r, shared<Mesh> m, GLtexture tex, GLprogram shader);
     DrawMesh(Render::MaterialPass* r, Mesh::FaceData& fd, Mesh::FaceIndex& fi, const char* texFile, GLprogram shader);
+    DrawMesh(Render::MaterialPass* r, Mesh::FaceData& fd, Mesh::FaceIndex& fi, GLtexture tex, GLprogram shader);
         
     void setup(const char* texFile, GLprogram shader);
+    void setup(GLtexture tex, GLprogram shader);
     void draw(const mat4& world);
 
     GLenum tesselPrim = GL_TRIANGLES;
